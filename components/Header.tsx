dd710a6fbc79518b2069e6dc0901727112df25f2
Bleyle <Bennybleylle09@gmail.com>
2023-09-17 18:00:20 +0300

    Add files via upload

---
+++ components/Header.tsx
@@ -0,0 +1,60 @@
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.svg'
+import Link from './Link'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between py-10">
+      <div>
+          <Link href='/' className="flex space-x-1">
+
+<div className="">
+<img src="Kick-Off-Logo-Alt.png" width="45" height="45" />
+</div>
+        <div className="uppercase font-logoFont text-sm font-bold ralative flex items-center">
+          {/* <span className="absolute top-5 left- text-4xl">tsn </span> */}
+
+          <span className="">the&nbsp;</span>
+          <span className="text-blue-500">Kick-Off</span>
+          <span className="">&nbsp;KE</span>
+        </div>
+          </Link>
+      {/* <Link href="/" aria-label={siteMetadata.headerTitle}>
+        <div className="flex items-center justify-between">
+          <div className="mr-3">
+            <Logo />
+          </div>
+          {typeof siteMetadata.headerTitle === 'string' ? (
+            <div className="hidden h-6 text-2xl font-semibold sm:block">
+              {siteMetadata.headerTitle}
+            </div>
+          ) : (
+            siteMetadata.headerTitle
+          )}
+        </div>
+      </Link> */}
+      </div>
+      <div className="flex items-center text-sm leading-5">
+        <div className="hidden sm:block">
+          {headerNavLinks.map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className="relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300 p-1 font-medium text-gray-900 dark:text-gray-100 sm:px-0 sm:mx-2"
+            >
+              {link.title}
+            </Link>
+          ))}
+        </div>
+        <ThemeSwitch />
+        <MobileNav />
+      </div>
+    </header>
+  )
+}
+
+export default Header
+
---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/Blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/hosts', title: 'Hosts' },
+  // { href: '/about', title: 'Host' },
+  { href: '/listen', title: 'Dive in' },
+]
+
+export default headerNavLinks
